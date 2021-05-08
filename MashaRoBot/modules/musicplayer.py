358126b56dd14a59e3db00a619617b56113b1541
Aquila-14 <78516399+Aquila-14@users.noreply.github.com>
2021-05-08 16:32:20 +0530

    Update musicplayer.py

--- MashaRoBot/modules/musicplayer.py
+++ MashaRoBot/modules/musicplayer.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__mod_name__ = "Music Player"
+__mod_name__ = "VC PLAYER"
 __help__ = """
           <b>》** EMCEEVCPLAYER ** 《</b>
             
