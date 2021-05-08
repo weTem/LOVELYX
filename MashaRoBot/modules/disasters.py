d3ec58919ce7d19117333f948ee1d4ff67737b5d
Aquila-14 <78516399+Aquila-14@users.noreply.github.com>
2021-05-08 16:42:18 +0530

    Update disasters.py

--- MashaRoBot/modules/disasters.py
+++ MashaRoBot/modules/disasters.py
@@ -678,7 +678,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "ACCESS LEVELS"
+__mod_name__ = "BOT ACCESS"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
