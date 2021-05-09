9dc79798937317ed194faf6dc9736c93ebe58dde
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-09 17:52:06 +0530

    Update AI_Chat.py

--- MashaRoBot/modules/AI_Chat.py
+++ MashaRoBot/modules/AI_Chat.py
@@ -44,6 +44,44 @@ daisyx = Client(
     sleep_threshold=5,
 )
 
+from MashaRobot import db as db_x
+
+lydia = db_x["CAHTBOT"]
+
+
+def add_chat(chat_id):
+    stark = lydia.find_one({"chat_id": chat_id})
+    if stark:
+        return False
+    else:
+        lydia.insert_one({"chat_id": chat_id})
+        return True
+
+
+def remove_chat(chat_id):
+    stark = lydia.find_one({"chat_id": chat_id})
+    if not stark:
+        return False
+    else:
+        lydia.delete_one({"chat_id": chat_id})
+        return True
+
+
+def get_all_chats():
+    r = list(lydia.find())
+    if r:
+        return r
+    else:
+        return False
+
+
+def get_session(chat_id):
+    stark = lydia.find_one({"chat_id": chat_id})
+    if not stark:
+        return False
+    else:
+        return stark
+
 def admins_only(func: Callable) -> Coroutine:
     async def wrapper(client: Client, message: Message):
         if message.from_user.id == OWNER_ID:
