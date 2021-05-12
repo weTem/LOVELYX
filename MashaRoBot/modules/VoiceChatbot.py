ff42f4db75b29d2e025881198c0e312c2bb3cbf5
Aquila-14 <78516399+Aquila-14@users.noreply.github.com>
2021-05-12 11:33:17 +0530

    Update VoiceChatbot.py

--- MashaRoBot/modules/VoiceChatbot.py
+++ MashaRoBot/modules/VoiceChatbot.py
@@ -5,7 +5,7 @@ import aiofiles
 import aiohttp
 from random import randint
 from pyrogram import filters
-from AsunaRobot import telethn as LYCIA
+from MashaRoBot import telethn as LYCIA
 
 async def fetch(url):
     async with aiohttp.ClientSession() as session:
