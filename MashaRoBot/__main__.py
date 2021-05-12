3c391fe31301e7fe86acc6e9ce6fc3c3dd3dfe30
Aquila-14 <78516399+Aquila-14@users.noreply.github.com>
2021-05-12 15:05:45 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -363,8 +363,8 @@ def Masha_about_callback(update: Update, context: CallbackContext):
                  ❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
                  ❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  ❍ I check for admins' permissions before executing any command and more stuffs
-                 \n_Masha's licensed under the GNU General Public License v3.0_
-                 Here is the [💾Repository](https://t.me/DaisyXNews).
+                 \n_Emcee's licensed under the GNU General Public License v3.0_
+                 Here is the [💾Repository](https://t.me/Emcee_Support).
                  If you have any question about Emcee, let us know at @Emceesupport.""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
