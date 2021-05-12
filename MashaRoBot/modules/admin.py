cdb6ec11923faa4bfe7a608a37e073044346c927
Aquila-14 <78516399+Aquila-14@users.noreply.github.com>
2021-05-12 22:29:27 +0530

    Update admin.py

--- MashaRoBot/modules/admin.py
+++ MashaRoBot/modules/admin.py
@@ -639,7 +639,14 @@ __help__ = """
  ❍ /title <title here>*:* sets a custom title for an admin that the bot promoted
  ❍ /admincache*:* force refresh the admins list
  ❍ /antispam <on/off/yes/no>*:* Will toggle our antispam tech or return your current settings.
-
+ ❍ /setgtitle <newtitle>*:* Sets new chat title in your group.
+ ❍ /setgpic*:* As a reply to file or photo to set group profile pic!
+ ❍ /delgpic*:* Same as above but to remove group profile pic.
+ ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
+ ❍ /setdescription <description>*:* Sets new chat description in group.
+ ❍ /zombies*:* Find all deleted accounts in your group.
+ ❍ /zombies clean*:* Remove all deleted accounts from your group.
+ 
 *Note:* Night Mode chats get Automatically closed at 12 am(IST)
 and Automatically openned at 6 am(IST) To Prevent Night Spams.
 
