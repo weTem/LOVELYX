d084fb0b978dbbc6bf45e94dad885aa5285f7651
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-14 18:14:18 +0530

    Create genstr.py

---
+++ MashaRoBot/modules/genstr.py
@@ -0,0 +1,152 @@
+import asyncio
+from asyncio.exceptions import TimeoutError
+
+from pyrogram import Client, filters
+from pyrogram.errors import (PhoneCodeExpired, PhoneCodeInvalid,
+                             PhoneNumberInvalid, SessionPasswordNeeded)
+
+from pyromod import listen 
+"""
+Credits:
+    This module belongs to https://github.com/AbirHasan2005/TG-String-Session/blob/main/genStr.py
+"""
+import traceback
+from functools import wraps
+from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
+from MashaRoBot import LOG_GROUP_ID, BOT_TOKEN, API_ID, API_HASH
+
+app = Client("DaisyX", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+
+def capture_err(func):
+    @wraps(func)
+    async def capture(client, message, *args, **kwargs):
+        try:
+            return await func(client, message, *args, **kwargs)
+        except ChatWriteForbidden:
+            await app.leave_chat(message.chat.id)
+            return
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type,
+                value=exc_obj,
+                tb=exc_tb,
+            )
+            error_feedback = split_limits(
+                "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
+                    0 if not message.from_user else message.from_user.id,
+                    0 if not message.chat else message.chat.id,
+                    message.text or message.caption,
+                    "".join(errors),
+                ),
+            )
+            for x in error_feedback:
+                await app.send_message(LOG_GROUP_ID, x)
+            raise err
+
+    return capture
+
+
+
+
+
+
+
+
+__MODULE__ = "GenStr"
+__HELP__ = """
+This Module Can Be Used To Generate Session String For A Userbot.
+Send **/genstr** Command To The Bot In Private And Follow Instructions."""
+
+
+@app.on_message(filters.command("genstr") & filters.private)
+@capture_err
+async def genstr(_, message):
+    chat = message.chat
+    while True:
+        number = await app.ask(
+            chat.id, "Send Your Phone Number In International Format."
+        )
+        if not number.text:
+            continue
+        phone = number.text.strip()
+        if phone.startswith("/"):
+            continue
+        confirm = await app.ask(
+            chat.id, f'`Is "{phone}" correct?` \n\nSend: `y`\nSend: `n`'
+        )
+        if confirm.text.startswith("/"):
+            continue
+        if "y" in confirm.text.lower():
+            break
+    try:
+        temp_client = Client(
+            ":memory:", api_id=6, api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e"
+        )
+    except Exception as e:
+        await app.send_message(chat.id, f"**ERROR:** `{str(e)}`")
+        return
+    try:
+        await temp_client.connect()
+    except ConnectionError:
+        await temp_client.disconnect()
+        await temp_client.connect()
+    try:
+        code = await temp_client.send_code(phone)
+        await asyncio.sleep(2)
+    except PhoneNumberInvalid:
+        await message.reply_text("Phone Number is Invalid")
+        return
+
+    try:
+        otp = await app.ask(
+            chat.id,
+            (
+                "An OTP is sent to your phone number, "
+                "Please enter OTP in `1 2 3 4 5` format. __(Space between each numbers!)__"
+            ),
+            timeout=300,
+        )
+
+    except TimeoutError:
+        await message.reply_text("Time limit reached of 5 min. Process Cancelled.")
+        return
+    otp_code = otp.text
+    try:
+        await temp_client.sign_in(
+            phone, code.phone_code_hash, phone_code=" ".join(str(otp_code))
+        )
+    except PhoneCodeInvalid:
+        await message.reply_text("Invalid OTP.")
+        return
+    except PhoneCodeExpired:
+        await message.reply_text("OTP is Expired.")
+        return
+    except SessionPasswordNeeded:
+        try:
+            two_step_code = await app.ask(
+                chat.id,
+                "Your account have Two-Step Verification.\nPlease enter your Password.",
+                timeout=300,
+            )
+        except TimeoutError:
+            await message.reply_text("Time limit reached of 5 min.")
+            return
+        new_code = two_step_code.text
+        try:
+            await temp_client.check_password(new_code)
+        except Exception as e:
+            await message.reply_text(f"**ERROR:** `{str(e)}`")
+            return
+    except Exception as e:
+        await app.send_message(chat.id, f"**ERROR:** `{str(e)}`")
+        return
+    try:
+        session_string = await temp_client.export_session_string()
+        await temp_client.disconnect()
+        await app.send_message(
+            chat.id, text=f"**HERE IS YOUR STRING SESSION:**\n```{session_string}```"
+        )
+    except Exception as e:
+        await app.send_message(chat.id, f"**ERROR:** `{str(e)}`")
+        return
