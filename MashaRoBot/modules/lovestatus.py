94ea069b27d9345d0c370e979dcdc6d91f955a6d
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-05-20 19:54:06 +0530

    Create lovestatus.py

---
+++ MashaRoBot/modules/lovestatus.py
@@ -0,0 +1,8 @@
+mod_name = "LOVE STATUS ❤️"    
+
+help = """ 
+Here is love sad ,romantic status , poetry , shayri , friendship etc...
+
+Join [LOVE STATUS❤️](https://t.me/shayri_of_loves) to see our post
+OWNED by   - @V3DMATOWNER 
+"""
