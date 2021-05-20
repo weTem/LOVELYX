1b7d27f4c85493f00616579879997d7129b05bda
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-05-20 21:19:42 +0530

    Update yt.py

--- MashaRoBot/modules/yt.py
+++ MashaRoBot/modules/yt.py
@@ -1,5 +1,13 @@
 __help__ = """
- - /yt <text>: perform a youtube search
- - /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+Here is help for downloading audio|video from YouTube
+- /ytaudio <link> 
+Example - /ytaudio https://youtu.be/i0_m90T04uw
+
+- /ytvideo <link> 
+Example - /ytvideo https://youtu.be/i0_m90T04uw
+
+SUPPORT [LOVE STATUS](https://t.me/shayri_of_loves
+
+MY [MASTER](https://t.me/V3DMATOWNER)
 """
-__mod_name__ = "Youtube 🎞"
+__mod_name__ = "YOUTUBE 🎞"
