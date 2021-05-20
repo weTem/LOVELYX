93f4403d9096a3795893a2ed5a022b4a83c20d9a
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-05-20 20:00:34 +0530

    Update lovestatus.py

--- MashaRoBot/modules/lovestatus.py
+++ MashaRoBot/modules/lovestatus.py
@@ -1,8 +1,9 @@
-mod_name = "LOVE STATUS ❤️"    
+#LOVE STATUS AND SHAYRI 
+#OWNED BY TUSHAR
 
+mod_name = "LOVE STATUS ❤️"    
 help = """ 
 Here is love sad ,romantic status , poetry , shayri , friendship etc...
-
 Join [LOVE STATUS❤️](https://t.me/shayri_of_loves) to see our post
 OWNED by   - @V3DMATOWNER 
 """
