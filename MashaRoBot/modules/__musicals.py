3b3d7822f0d7cae91df89a4711e5ab7bc96c94ba
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-05-20 10:39:30 +0530

    Update __musicals.py

--- MashaRoBot/modules/__musicals.py
+++ MashaRoBot/modules/__musicals.py
@@ -2,6 +2,8 @@ __mod_name__ = "MUSIC"
 
 __help__ = """
  ❍ /audio <song name>*:* Uploads the Audio in it's best quality available
- 💡Ex: `/song Faded Alan Walker`
+ 💡Ex: `/audio Faded Alan Walker`
+❍ /song <song name>*:* Uploads the Audio in it's best quality available
+ 💡Ex: `/song teri mitti`
  ❍ /saavn <song name>*:* Download song from saavn.
 """
