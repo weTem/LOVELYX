055e2f058295aea65d11f2aa54cb57feaa4d8a05
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-05-22 07:09:14 +0530

    Update userinfo.py

--- MashaRoBot/modules/userinfo.py
+++ MashaRoBot/modules/userinfo.py
@@ -290,7 +290,7 @@ def info(update: Update, context: CallbackContext):
         text += "\n\nThe Disaster level of this person is 'Superior'."
         disaster_level_present = True
     elif user.id in DEV_USERS:
-        text += "\n\nThis user is member of 'Emcee powerhouse'."
+        text += "\n\nThis user is member of 'lovely powerhouse'."
         disaster_level_present = True
     elif user.id in DRAGONS:
         text += "\n\nThe Disaster level of this person is 'Legend'."
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/EMCEE_UPDATES/3">?</a>]'.format(
+        text += ' [<a href="https://t.me/LOVELYUPDATES/2">?</a>]'.format(
             bot.username
         )
 
