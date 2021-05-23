9fc9a719ad336fa0f687964eb88420691875527b
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-05-23 23:50:40 +0530

    Update disasters.py

--- MashaRoBot/modules/disasters.py
+++ MashaRoBot/modules/disasters.py
@@ -641,7 +641,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "BOT ACCESS"
+__mod_name__ = "BOT ACCESS⚡"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
