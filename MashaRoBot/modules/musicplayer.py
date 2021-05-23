31777821a5b6b08b1cb5772ada674c9a48f938c3
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-05-24 00:01:50 +0530

    Update musicplayer.py

--- MashaRoBot/modules/musicplayer.py
+++ MashaRoBot/modules/musicplayer.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__mod_name__ = "VC PLAYER"
+__mod_name__ = "VC PLAYER🎧"
 __help__ = """
           <b>》** @Lovelyvcrobot ** 《</b>
             
