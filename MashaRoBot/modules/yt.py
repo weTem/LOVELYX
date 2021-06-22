ca225a0f0b73503d3d67727d65d9543dfe91c743
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-06-22 22:40:17 +0530

    Update yt.py

--- MashaRoBot/modules/yt.py
+++ MashaRoBot/modules/yt.py
@@ -1,11 +1,12 @@
 __help__ = """
 Here is help for downloading audio|video from YouTube
 - /ytaudio <link> 
-Example - /ytaudio https://youtu.be/i0_m90T04uw
+Example - "/ytaudio https://youtu.be/i0_m90T04uw"
+Aᴜᴅɪᴏ ɪsɴ'ᴛ sᴜᴘᴘᴏʀᴛɪɴɢ , ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴠɪᴅᴇᴏ
 
 - /ytvideo <link> 
-Example - /ytvideo https://youtu.be/i0_m90T04uw
+Example - "/ytvideo https://youtu.be/i0_m90T04uw"
 
 SUPPORT [LOVE STATUS❤️](https://t.me/shayri_of_loves
 """
-__mod_name__ = "YOUTUBE 🎞"
+__mod_name__ = "ʏᴏᴜᴛᴜʙᴇ🎞"
