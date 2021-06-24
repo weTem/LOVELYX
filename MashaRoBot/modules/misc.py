afd24e40ae86784f203d5adac827dec4f54c6080
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-06-24 19:39:32 +0530

    Update misc.py

--- MashaRoBot/modules/misc.py
+++ MashaRoBot/modules/misc.py
@@ -111,7 +111,7 @@ MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help)
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 
-__mod_name__ = "EXTRAS👽"
+__mod_name__ = "ᴇxᴛʀᴀs👽"
 __command_list__ = ["id", "echo"]
 __handlers__ = [
     ECHO_HANDLER,
