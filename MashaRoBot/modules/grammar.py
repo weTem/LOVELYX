6086b83fc1d71c67034781f1e84f8e6a341b5a23
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-06-24 19:34:03 +0530

    Update grammar.py

--- MashaRoBot/modules/grammar.py
+++ MashaRoBot/modules/grammar.py
@@ -52,7 +52,7 @@ __help__ = """
  - /t: while replying to a message, will reply with a grammar corrected version
 """
 
-__mod_name__ = "GRAMMAR"
+__mod_name__ = "ɢʀᴀᴍᴍᴀʀ🙇"
 
 
 TRANSLATE_HANDLER = CommandHandler('t', translate)
