ef760cbeb64f390bd552d2bd9e0e33c9c65aea69
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-07-05 10:51:47 +0530

    Update _pyroforcesub.py

--- MashaRoBot/modules/_pyroforcesub.py
+++ MashaRoBot/modules/_pyroforcesub.py
@@ -12,9 +12,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from MASHAROBOT import DRAGONS as SUDO_USERS
-from MASHAROBOT import pbot
-from MASHAROBOT.modules.sql_extended import forceSubscribe_sql as sql
+from MashaRobot import DRAGONS as SUDO_USERS
+from MashaRobot import pbot
+from MashaRobot.modules.sql_extended import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 
