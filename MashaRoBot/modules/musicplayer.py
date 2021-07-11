896042bb3473a84a518075538ecae38c77beb0bd
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-07-11 13:45:47 +0530

    Update musicplayer.py

--- MashaRoBot/modules/musicplayer.py
+++ MashaRoBot/modules/musicplayer.py
@@ -1,23 +1,9 @@
-# Copyright (C) 2021 TeamDaisyX
+#LOVE STATUS AND SHAYRI 
+#OWNED BY TUSHAR
 
-
-# This file is part of Daisy (Telegram Bot)
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-__help__ = """
-          <b>》** @LOVELYR_OBOT ** 《</b>
+mod_name = "Vᴄ ᴘʟᴀʏᴇʀ🎧"
+help = """
+    <b>》 @LOVELYR_OBOT  《</b>
             
  @Lovelyvcrobot plays music in your group's voice chat
  
@@ -26,7 +12,7 @@ __help__ = """
 <b> Setting up </b>
 1) Make bot admin
 2) Start a voice chat
-3) Try `/play [song name]` for the first time by an admin
+3) Try /play [song name] for the first time by an admin
 *) If userbot joined enjoy music, If not add @LOVELYVC to your group and retry
 <b> Commands </b>
 =>> Song Playing 🎶
@@ -49,5 +35,3 @@ __help__ = """
 *Player cmd and all other cmds except /play, /current  and /playlist  are only for admins with manage group
 <b> PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME </b>
 """
-
-__mod_name__ = "Vᴄ ᴘʟᴀʏᴇʀ🎧"
