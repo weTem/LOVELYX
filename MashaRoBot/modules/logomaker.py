7217e91f66be08fd3d106dc5430e0d5356b587c6
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-09-14 09:41:35 +0530

    Update logomaker.py

--- MashaRoBot/modules/logomaker.py
+++ MashaRoBot/modules/logomaker.py
@@ -98,7 +98,9 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "LogoByInnexia.png"
+    fname2 = "LogoByLovely
+
+.png"
     img.save(fname2, "png")
     await tbot.send_file(event.chat_id, fname2, caption="💕 Made By @LOVELYR_OBOT Support @LOVELYAPPEAL")
     if os.path.exists(fname2):
